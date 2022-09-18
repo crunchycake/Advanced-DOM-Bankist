fd69fdd71469633b47cfd1606c56e1f12f2ab378
Mateusz Cieślik <mateusz.frontend@gmail.com>
2022-09-18 14:28:08 +0200

    styles and attributes

--- script.js
+++ script.js
@@ -70,3 +70,28 @@ header.append(message);
 document.querySelector('.btn--close-cookie').addEventListener('click', function () {
     message.remove();
 })
+
+
+// Styles
+
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+ message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+ document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+
+// Attributes
+
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.designer);
+
+// pobieranie atrybutu
+console.log(logo.getAttribute('designer'));
+
+logo.setAttribute('company', 'Bankist');
+
+
